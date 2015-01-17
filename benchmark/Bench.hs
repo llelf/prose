f5079b5dc2c09d4b5fc02031d3875bbbc8e54165
Antonio Nikishaev <me@lelf.lu>
2015-01-17 08:45:05 +0000

    bench: 5k/2.5k

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -6,14 +6,17 @@ import Prose.Segmentation.Graphemes as Graph
 import Data.Text as T
 import Data.Text.ICU as ICU
 
-str_english = unsafePerformIO $ readFile "benchmark/english.txt"
+str_english = unsafePerformIO $ readFile "benchmark/english-2.5k.txt"
+str_japanese = unsafePerformIO $ readFile "benchmark/japanese-5k.txt"
 
 charBreak = ICU.breakCharacter ICU.Root
 
 
 main = defaultMain [
         bgroup "seg/graph" [
-         bench "A" (nf Graph.segment str_english),
-         bench "T" (nf (ICU.breaks charBreak) (T.pack str_english))
+         bench "P/E" (nf Graph.segment str_english),
+         bench "P/J" (nf Graph.segment str_japanese),
+         bench "ICU/E" (nf (ICU.breaks charBreak) (T.pack str_english)),
+         bench "ICU/J" (nf (ICU.breaks charBreak) (T.pack str_japanese))
         ]
        ]
