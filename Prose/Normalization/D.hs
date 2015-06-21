a985c4770777aee82ab49b13626e437b3e283024
Antonio Nikishaev <me@lelf.lu>
2015-06-21 21:21:32 +0000

    Prose.Normalization.D: fully decompose & reorder. Now works

--- Prose/Normalization/D.hs
+++ Prose/Normalization/D.hs
@@ -1,13 +1,38 @@
-{-# LANGUAGE ViewPatterns #-}
 module Prose.Normalization.D where
 
-import qualified Prose.Properties.Decomp as D
+import qualified Prose.Properties.DecompD as D
+import qualified Prose.Properties.CombiningClass as CC
 import qualified Data.Map as M
+import qualified Data.List.Sequences as ListSeq
+import Control.Monad (ap)
+import Data.List
+import Data.Ord
 
 decomposeChar :: Char -> [Char]
-decomposeChar c | Just ds <- M.lookup c D.decomp = ds
-                | otherwise                      = [c]
+decomposeChar c = M.findWithDefault [c] c D.decompd
+
+decomposeFully :: Char -> [Char]
+decomposeFully = fst . head
+                     . dropWhile (uncurry (/=))
+                     . ap zip tail
+                     . iterate (concatMap decomposeChar)
+                     . (:[])
+
+
+
+combiningClassOf :: Char -> Int
+combiningClassOf c = M.findWithDefault 0 c CC.combiningclass
+
+
 
 decompose :: [Char] -> [Char]
-decompose = concatMap decomposeChar
+decompose = map fst
+            . concatMap (sortBy (comparing snd))
+            . ListSeq.splitSeq brk
+            . map (ap (,) combiningClassOf)
+            . concatMap decomposeFully
+    where brk (_,cca) (_,0) | cca/=0 = False
+          brk _ _                    = True
+
+
 
