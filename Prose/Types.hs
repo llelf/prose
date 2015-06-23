d491674fde583a2196cb80e383f097cde88e6c93
Antonio Nikishaev <me@lelf.lu>
2015-06-23 15:12:50 +0000

    P.Types: Show, IsString

--- Prose/Types.hs
+++ Prose/Types.hs
@@ -1,8 +1,10 @@
 module Prose.Types (CodePoint,Grapheme,Prose(..))
     where
 
-import Data.Text (Text)
-import Data.ByteString (ByteString)
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
+import Data.String
+import Data.Monoid
 
 -- | Unicode code point
 type CodePoint = Char
@@ -15,4 +17,14 @@ data Prose = T T.Text             -- ^ Text's utf-16 data in NFC
            -- | U BS.ByteString       -- ^ ByteString with utf-8 in NFC
 
            -- ^ Well…ideally, change Text’s internal represetation to utf-8
+
+
+-- TODO: NF
+
+instance Show Prose where
+    show (T txt) = "\"" <> T.unpack txt <> "\"" -- XXX only printable
+
+instance IsString Prose where
+    fromString = T . T.pack
+
 
