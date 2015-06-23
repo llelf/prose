c621041fc6c1f5220c1b3cd154083516bbf2915f
Antonio Nikishaev <me@lelf.lu>
2015-06-23 15:13:01 +0000

    Prose.graphemes

--- Prose.hs
+++ Prose.hs
@@ -2,3 +2,11 @@
 module Prose (Prose) where
 
 import Prose.Types
+import qualified Prose.Segmentation.Graphemes as Gr
+import qualified Data.Text as T
+
+graphemes :: Prose -> [Prose]
+graphemes (T txt) = map (T . T.pack) . Gr.segment . T.unpack $ txt
+
+
+
