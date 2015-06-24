01e40c5e2e6992fec07e241ae2819dd2473cd18c
Antonio Nikishaev <me@lelf.lu>
2015-06-24 12:23:48 +0000

    P.Normalization: decompseD, decompseKD

--- Prose/Normalization.hs
+++ Prose/Normalization.hs
@@ -1,6 +1,7 @@
 module Prose.Normalization where
 
-import qualified Prose.Properties.DecompD as D
+import qualified Prose.Properties.DecompD as NFD
+import qualified Prose.Properties.DecompKD as NFKD
 import qualified Prose.Properties.CombiningClass as CC
 import qualified Data.Map as M
 import qualified Data.List.Sequences as ListSeq
@@ -8,14 +9,18 @@ import Control.Monad (ap)
 import Data.List
 import Data.Ord
 
-decomposeChar :: Char -> [Char]
-decomposeChar c = M.findWithDefault [c] c D.decompd
+type DecompMap = M.Map Char [Char]
+newtype Decomposed = Decomposed { unDecomposed :: [Char] }
 
-decomposeFully :: Char -> [Char]
-decomposeFully = fst . head
+decomposeChar :: DecompMap -> Char -> [Char]
+decomposeChar mp c = M.findWithDefault [c] c mp
+
+
+decomposeFully :: DecompMap -> Char -> Decomposed
+decomposeFully mp = Decomposed . fst . head
                      . dropWhile (uncurry (/=))
                      . ap zip tail
-                     . iterate (concatMap decomposeChar)
+                     . iterate (concatMap (decomposeChar mp))
                      . (:[])
 
 
@@ -24,13 +29,17 @@ combiningClassOf :: Char -> Int
 combiningClassOf c = M.findWithDefault 0 c CC.combiningclass
 
 
+decomposeD, decomposeKD :: [Char] -> [Char]
+decomposeD  = decompose NFD.decompd
+decomposeKD = decompose NFKD.decompkd
 
-decompose :: [Char] -> [Char]
-decompose = map fst
+
+decompose :: DecompMap -> [Char] -> [Char]
+decompose mp = map fst
             . concatMap (sortBy (comparing snd))
             . ListSeq.splitSeq brk
             . map (ap (,) combiningClassOf)
-            . concatMap decomposeFully
+            . concatMap (unDecomposed . decomposeFully mp)
     where brk (_,cca) (_,0) | cca/=0 = False
           brk _ _                    = True
 
