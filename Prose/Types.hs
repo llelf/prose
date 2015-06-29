3f6ce7cf140d52af6fe7fc5b773e3c5c92eb45bb
Antonio Nikishaev <me@lelf.lu>
2015-06-29 14:48:01 +0000

    Eq Prose

--- Prose/Types.hs
+++ Prose/Types.hs
@@ -3,8 +3,10 @@ module Prose.Types (CodePoint,Grapheme,Prose(..))
 
 import qualified Data.Text as T
 import qualified Data.ByteString as BS
+import qualified Prose.Normalization.Text as Norm
 import Data.String
 import Data.Monoid
+import Data.Function
 
 -- | Unicode code point
 type CodePoint = Char
@@ -13,13 +15,15 @@ type CodePoint = Char
 type Grapheme = [Char]
 
 
-data Prose = T T.Text             -- ^ Text's utf-16 data in NFC
-           -- | U BS.ByteString       -- ^ ByteString with utf-8 in NFC
+data Prose = T { unT :: T.Text } -- ^ Text's utf-16 data
+           -- | U BS.ByteString       -- ^ ByteString with utf-8
 
            -- ^ Well…ideally, change Text’s internal represetation to utf-8
 
 
--- TODO: NF
+
+instance Eq Prose where
+    (==) = (==) `on` Norm.decomposeD . unT
 
 instance Show Prose where
     show (T txt) = "\"" <> T.unpack txt <> "\"" -- XXX only printable
