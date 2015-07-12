54f57828d49bec30c33f534bec65fd04f635ea69
Antonio Nikishaev <me@lelf.lu>
2015-07-12 20:50:12 +0000

    forgotten tests/Tests.hs

---
+++ tests/Tests.hs
@@ -0,0 +1,26 @@
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Test.Tasty.HUnit
+
+import qualified NormalizationTests as Norm
+import qualified SegmentationTests as Segm
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "tests" [
+         testGroup "norm" [
+                        testCase "g1" (Norm.g1_check @?= True)
+                       ],
+         testGroup "segm" [
+            testGroup "graph" [
+               testProperty "g1" Segm.g1_prop,
+               testCase "g2" (Segm.g2_check @?= True)
+            ],
+            testGroup "word" [
+               testCase "w1" (Segm.w1_check @?= True)
+            ]
+        ]
+       ]
+
+
