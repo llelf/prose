22c6f7e911d8e216e715ac0e6278bc5963a8d53f
Antonio Nikishaev <me@lelf.lu>
2015-07-16 15:50:23 +0000

    more benchmarks

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,27 +1,52 @@
 
 import Criterion.Main
 import System.IO.Unsafe
+import qualified Prose.Segmentation.Graphemes as Graph
+import qualified Prose.Segmentation.Words as Word
+import qualified Prose.Normalization as Norm
+import qualified Data.Text as T
+import qualified Data.Text.ICU as ICU
+import Control.DeepSeq (NFData)
+import Data.Monoid
 import Prose
-import Prose.Segmentation.Graphemes as Graph
-import Data.Text as T
-import Data.Text.ICU as ICU
 
 load = unsafePerformIO . readFile . ("benchmark/"++)
 
 str_english  = load "english-2.5k.txt"
 str_japanese = load "japanese-5k.txt"
 str_chars    = load "characters.txt"
+str_zalgo    = load "zalgo.txt"
+
+testData = [("En",str_english), ("Ja",str_japanese), ("Chr",str_chars), ("Z",str_zalgo)]
 
 charBreak = ICU.breakCharacter ICU.Root
+wordBreak = ICU.breakWord ICU.Root
 
+
+benchBoth :: (NFData r, NFData q)
+             => String -> (T.Text -> r) -> (String -> q) -> String -> [Benchmark]
+benchBoth name icu prose str = [
+   bench (name<>"-icu") (nf icu (T.pack str)),
+   bench (name<>"-p")   (nf prose str)
+ ]
+
+benchThese :: (NFData r, NFData q)
+              => (T.Text -> r) -> (String -> q) -> [Benchmark]
+benchThese icu prose = concat [ benchBoth name icu prose str | (name,str) <- testData ]
 
 main = defaultMain [
-        bgroup "seg/graph" [
-         bench "P/E" (nf Graph.segment str_english),
-         bench "P/J" (nf Graph.segment str_japanese),
-         bench "P/C" (nf Graph.segment str_chars),
-         bench "ICU/E" (nf (ICU.breaks charBreak) (T.pack str_english)),
-         bench "ICU/J" (nf (ICU.breaks charBreak) (T.pack str_japanese)),
-         bench "ICU/C" (nf (ICU.breaks charBreak) (T.pack str_chars))
+        bgroup "seg"
+        [
+           bgroup "graph" $ benchThese (ICU.breaks charBreak) Graph.segment,
+           bgroup "words" $ benchThese (ICU.breaks wordBreak) Word.segment
+          -- bgroup "wordsT" $ benchThese (ICU.breaks wordBreak) Word.segmentT
+        ],
+
+        bgroup "norm"
+        [
+           bgroup "nfd"  $ benchThese (ICU.normalize ICU.NFD) Norm.decomposeD,
+           bgroup "nfkd" $ benchThese (ICU.normalize ICU.NFKD) Norm.decomposeKD,
+           bgroup "nfc"  $ benchThese (ICU.normalize ICU.NFC) Norm.composeC
         ]
+
        ]
