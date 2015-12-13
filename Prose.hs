a1d90570055ce9d081e7087d33c33aaf37dcd42a
Antonio Nikishaev <me@lelf.lu>
2015-12-13 15:55:51 +0000

    Prose: words, norm{C,D,KD}; export

--- Prose.hs
+++ Prose.hs
@@ -8,14 +8,30 @@
 --
 --
 
-module Prose (Prose) where
+module Prose (Prose,
+              graphemes, words,
+              normC, normD, normKD) where
 
+import Prelude hiding (words)
 import Prose.Types
 import qualified Prose.Segmentation.Graphemes as Gr
+import qualified Prose.Segmentation.Words as Wd
+import qualified Prose.Normalization.Text as Nm
 import qualified Data.Text as T
 
 graphemes :: Prose -> [Prose]
 graphemes (T txt) = map (T . T.pack) . Gr.segment . T.unpack $ txt
 
+words :: Prose -> [Prose]
+words (T txt) = map T . Wd.segmentT $ txt
+
+normC :: Prose -> Prose
+normC (T txt) = T . Nm.composeC $ txt
+
+normD :: Prose -> Prose
+normD (T txt) = T . Nm.decomposeD $ txt
+
+normKD :: Prose -> Prose
+normKD (T txt) = T . Nm.decomposeKD $ txt
 
 
